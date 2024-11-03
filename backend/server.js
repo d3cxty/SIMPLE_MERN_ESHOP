665b319c8bf7b196e4069793c9dd2423003d8baa
d3cxty <hirwapie@gmail.com>
2024-11-03 07:08:51 +0200

    finaly decided o let go react and used simple html

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import {connectDB} from './config/db.js'
 import router from './routes/Product.router.js'
 const app = express();
+import cors from 'cors';
+app.use(cors());
+
 
 app.use('/', router);
 // Connect to MongoDB
